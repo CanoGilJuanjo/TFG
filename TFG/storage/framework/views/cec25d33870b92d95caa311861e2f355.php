2477e0c84a6b6363e7f25ebb0c4d3d91d4827896
CanoGilJuanjo <canogiljuanjose@gmail.com>
2024-05-01 22:56:56 +0200

    Eliminados todos los gitignore

---
+++ TFG/storage/framework/views/cec25d33870b92d95caa311861e2f355.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php echo e(config("app.name")); ?></title>
+        <link rel="shortcut icon" href="<?php echo e(asset('favicon.ico')); ?>" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <!-- Importamos REACT -->
+        <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+        <?php echo app('Illuminate\Foundation\Vite')('resources/react/app.tsx'); ?>
+    </head>
+    <body>
+        <div id="app" data-projects=""></div>
+    </body>
+</html><?php /**PATH C:\Users\pollo\Downloads\TFG\resources\views/index.blade.php ENDPATH**/ ?>
