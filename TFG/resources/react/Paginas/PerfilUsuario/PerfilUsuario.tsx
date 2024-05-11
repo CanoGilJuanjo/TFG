45e0f1c0bb774d18ad1b1953bdd3a904d3163acb
CanoGilJuanjo <canogiljuanjose@gmail.com>
2024-05-11 17:56:48 +0200

    Formato de muestra historico cambiado

--- TFG/resources/react/Paginas/PerfilUsuario/PerfilUsuario.tsx
+++ TFG/resources/react/Paginas/PerfilUsuario/PerfilUsuario.tsx
@@ -1,5 +1,5 @@
 import { EditIcon, EmailIcon, StarIcon } from '@chakra-ui/icons'
-import { Button, Card, CardBody, CardFooter, Center, Heading, Stack,Text, Image} from '@chakra-ui/react'
+import { Button, Card, CardBody, CardFooter, Center, Heading, Stack, Text, Image } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 
 export const PerfilUsuario = () => {
@@ -9,82 +9,94 @@ export const PerfilUsuario = () => {
     nombre: "Pedro",
     apellido: "Pruebas",
     ubicacion: "Malaga,España",
-    mail:"pedropruebas@gmail.com",
-    historial:[
+    mail: "pedropruebas@gmail.com",
+    historial: [
       {
-        evento:"Rosse",
-        imagen:"media/evento.png", 
-        precio:"12,60",
-        fechaCompra:"28/04/2024",
-        descripcion:"Evento invernal en la discoteca Rosse"
+        evento: "Rosse",
+        imagen: "media/evento.png",
+        precio: "12,60",
+        fechaCompra: "28/04/2024",
+        descripcion: "Evento invernal en la discoteca Rosse"
+      },
+      {
+        evento: "Laguna Seca",
+        imagen: "media/evento.png",
+        precio: "70,40",
+        fechaCompra: "12/01/2024",
+        descripcion: "3 tandas completas al circuito Laguna Seca"
       }
     ]
   }
 
-  const Carta = ({historial}) =>{
-    return(
+  const Carta = ({ historial }) => {
+    return (
       <Card
         direction={{ base: 'column', sm: 'row' }}
         overflow='hidden'
         variant='outline'
-        width={"90%"}
+        width={"60vw"}
+        height={"20vh"}
+        marginBottom={"5px"}
       >
         <Image width={"50vh"} src={historial.imagen} alt={historial.evento}></Image>
 
         <Stack>
           <CardBody>
-            <Heading size='md'>{historial.evento}</Heading>
+            <Heading size='md'>{historial.evento} - {historial.precio}€</Heading>
             <Text py='2'>
-              {historial.descripcion}
+              Descripcion: {historial.descripcion}
+            </Text>
+            <Text>
+              Fecha compra: {historial.fechaCompra}
             </Text>
           </CardBody>
         </Stack>
       </Card>
     )
   }
-  
-  const [historialCompras,setHistorialCompra] = useState([])
-  const [nombre,setNombre] = useState("")
-  const [apellido,setApellido] = useState("")
-  const [ubicacion,setUbicacion] = useState("")
-  const [mail,setMail] = useState("")
 
-  useEffect(()=>{
+  const [historialCompras, setHistorialCompra] = useState([])
+  const [nombre, setNombre] = useState("")
+  const [apellido, setApellido] = useState("")
+  const [ubicacion, setUbicacion] = useState("")
+  const [mail, setMail] = useState("")
+
+  useEffect(() => {
     setNombre(consulta.nombre)
     setApellido(consulta.apellido)
     setMail(consulta.mail)
     setUbicacion(consulta.ubicacion)
-  },[])
+  }, [])
 
   return (
     <>
-      <div style={{maxWidth:"600px",margin:"20px auto",padding:"20px",border:"1px solid #ccc",borderRadius:"10px",boxShadow:"1px 1px 8px black"}}>
+      <div style={{ maxWidth: "600px", margin: "20px auto", padding: "20px", border: "1px solid #ccc", borderRadius: "10px", boxShadow: "1px 1px 8px black" }}>
         <img style={{
-          width:"150px",height:"150px",borderRadius:"50%",margin:"0 auto 20px",display:"block"
-        }} src="media/user.png" alt="Foto de perfil"/>
+          width: "150px", height: "150px", borderRadius: "50%", margin: "0 auto 20px", display: "block"
+        }} src="media/user.png" alt="Foto de perfil" />
         <div style={{
-          textAlign:"center",
-          display:"flex",
-          flexFlow:"column"
+          textAlign: "center",
+          display: "flex",
+          flexFlow: "column"
         }}>
           <h2 style={{
-            margin:"0",fontSize:"24px",marginBottom:"10px"
+            margin: "0", fontSize: "24px", marginBottom: "10px"
           }}>{nombre} {apellido}</h2>
-          <p id='mail' style={{margin:"5px 0"}}><EmailIcon></EmailIcon> : {mail}</p>
-          <p style={{margin:"5px 0"}}><StarIcon></StarIcon> : {ubicacion}</p>
+          <p id='mail' style={{ margin: "5px 0" }}><EmailIcon></EmailIcon> : {mail}</p>
+          <p style={{ margin: "5px 0" }}><StarIcon></StarIcon> : {ubicacion}</p>
         </div>
       </div>
-      
+
       <Center>
-        <h2 style={{fontSize:"xx-large"}}>Historial de compras</h2>
+        <h2 style={{ fontSize: "xx-large" }}>Historial de compras</h2>
       </Center>
       <Center>
-          <div style={{display:"flex", flexFlow:"column wrap"}}>
-            {consulta.historial.map(respuesta =>{
-              return(<Carta historial={respuesta}/>)
-            })}
-          </div>
-          </Center>
+        <div style={{ display: "flex", flexFlow: "column wrap" }}>
+          {consulta.historial.map(respuesta => {
+            return (<Carta historial={respuesta} />)
+          })}
+        </div>
+      </Center>
     </>
   )
 }
