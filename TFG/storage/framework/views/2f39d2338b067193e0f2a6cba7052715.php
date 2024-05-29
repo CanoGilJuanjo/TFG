cdb2ed814d1b6f0520612e111e1a64050d8a29e9
CanoGilJuanjo <canogiljuanjose@gmail.com>
2024-05-29 23:19:33 +0200

    version 0.5 suscripciones

--- TFG/storage/framework/views/2f39d2338b067193e0f2a6cba7052715.php
+++ TFG/storage/framework/views/2f39d2338b067193e0f2a6cba7052715.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config("app.name") }}</title>
-        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+        <title><?php echo e(config("app.name")); ?></title>
+        <link rel="shortcut icon" href="<?php echo e(asset('favicon.ico')); ?>" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <!-- Estilos de Mapas #SE TIENE QUE ELIMINAR DE ESTA PAGINA, SOLO SE USA EN LAS PAGINAS QUE LO NECESITEN# -->
@@ -12,10 +12,10 @@
         <!-- Bootstrap -->
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
         <!-- Importamos REACT -->
-        @viteReactRefresh
-        @vite('resources/react/app.tsx')
+        <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+        <?php echo app('Illuminate\Foundation\Vite')('resources/react/app.tsx'); ?>
     </head>
     <body>
         <div id="app" data-projects=""></div>
     </body>
-</html>
+</html><?php /**PATH C:\Users\pollo\Desktop\Trabajo\TFG\resources\views/subscripciones.blade.php ENDPATH**/ ?>
