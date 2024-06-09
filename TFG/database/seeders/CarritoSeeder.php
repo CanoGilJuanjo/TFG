0dd621138303c84e0b964c00a69b2470379ba673
JNaor12 <guannaor@gmail.com>
2024-06-10 01:07:29 +0200

    Carrito Seeder

---
+++ TFG/database/seeders/CarritoSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CarritoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("carrito")->insert([
+            [
+                "id" => 1,
+                "precio_total" => 0.00,
+                "id_usuario" => 1,
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 2,
+                "precio_total" => 0.00,
+                "id_usuario" => 2,
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+        ]);
+    }
+}
